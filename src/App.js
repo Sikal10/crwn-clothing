079377415e8f92fb23bc5ffa96ecd654c2a0f3b0
Sikal Sikal <sikal.sikal.ss@gmail.com>
2020-01-08 17:38:25 +0100

    updated our application App component, sign-in component and sign-up component to use hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -15,19 +15,11 @@ import Header from './components/header/header.component';
 import { selectCurrentUser } from './redux/user/user.selector';
 import {checkUserSession} from "./redux/user/user.actions";
 
-class App extends React.Component {
-    unsubscribeFromAuth = null;
+const App = ({ checkUserSession, currentUser }) => {
+    useEffect(() => {
+        checkUserSession()
+    }, [checkUserSession]);
 
-    componentDidMount() {
-        const { checkUserSession } = this.props;
-        checkUserSession();
-    }
-
-    componentWillUnmount() {
-        this.unsubscribeFromAuth();
-    }
-
-    render() {
         return (
             <div>
                 <Header />
@@ -39,7 +31,7 @@ class App extends React.Component {
                         exact
                         path='/signin'
                         render={() =>
-                            this.props.currentUser ? (
+                            currentUser ? (
                                 <Redirect to='/' />
                             ) : (
                                 <SignInAndSignUpPage />
@@ -49,8 +41,8 @@ class App extends React.Component {
                 </Switch>
             </div>
         );
-    }
-}
+    };
+
 
 const mapStateToProps = createStructuredSelector({
     currentUser: selectCurrentUser
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Route } from 'react-router-dom';
 import {connect} from 'react-redux';
 
@@ -8,15 +8,10 @@ import { fetchCollectionsStart} from "../../redux/shop/shop.actions";
 
 import CollectionPageContainer from "../collection/collection.container";
 
-class  ShopPage extends React.Component {
-
-    componentDidMount() {
-        const {fetchCollectionsStart} = this.props;
-        fetchCollectionsStart()
-    }
-
-    render() {
-        const {match} = this.props;
+const ShopPage = ({fetchCollectionsStart, match}) => {
+    useEffect(() => {
+        fetchCollectionsStart();
+    }, [fetchCollectionsStart]);
 
         return (
           <div className='shop-page'>
@@ -31,8 +26,7 @@ class  ShopPage extends React.Component {
                />
            </div>
      )
-    }
-}
+    };
 
 const mapDispatchToProps = dispatch => ({
     fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
