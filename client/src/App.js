9ad5b09bd3ba838b8ab04e36b481a08a4bb1e24b
Sikal Sikal <sikal.sikal.ss@gmail.com>
2020-01-09 08:36:22 +0100

    separating our cra app into client folder, added express server to help handle backend stripe payment

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+ import React, {useEffect} from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
